7b3b9b30ca34abd4e604d992ffa16925506fb7e0
nikolastefanov <nikolay_stefanov1965@abv.bg>
2021-07-27 18:26:45 +0300

    IssueType all pages

--- CarService/CarService/Infrastructure/ApplicationBuilderExtensions.cs
+++ CarService/CarService/Infrastructure/ApplicationBuilderExtensions.cs
@@ -36,7 +36,7 @@ namespace CarService.Infrastructure
                        new IssueType { Name = "Diagnostics" ,ImageUrl="https://previews.123rf.com/images/nitroxelmares/nitroxelmares1604/nitroxelmares160400021/55664614-car-diagnostics-test-service-illustration-isolated-on-white.jpg "},
                        new IssueType { Name = "Brake system" ,ImageUrl="https://www.anatomynote.com/wp-content/uploads/2019/05/6189/Car-brake-system-diagram.jpg "},
                        new IssueType { Name = "Electrical system" ,ImageUrl="https://www.newkidscar.com/wp-content/uploads/2019/04/electrical-system.jpg "},
-                       new IssueType { Name = "Coupe" ,ImageUrl="https://fs3.ebait.biz/c4RpU5aF/review_337244_1.jpg "},
+                       new IssueType { Name = "Body" ,ImageUrl="https://us.123rf.com/450wm/nosua/nosua1609/nosua160900260/63522079-concept-car-painted-red-body-and-primed-parts-near-isolated-on-white-background-3d-render.jpg?ver=6"},
                        new IssueType { Name = "Tires " ,ImageUrl="https://www.napolitan.it/wp-content/uploads/2019/06/pneumatici-4-stagioni-300x260.jpg"},
                          new IssueType { Name = "air conditioner" ,ImageUrl=" https://media.istockphoto.com/photos/car-air-conditioner-system-in-car-garage-car-air-conditioner-check-picture-id1179826380"},
                          new IssueType { Name = "Technical inspeccion" ,ImageUrl="https://www.theolivepress.es/wp-content/uploads/2020/01/itvvv.jpg?width=1200&enable=upscale"},
